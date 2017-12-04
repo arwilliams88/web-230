2d134d34d7c7e1c0d15211bf24d2648c8cf92f19
Andrea Williams <andwilliams@bellevue.edu>
2017-12-03 21:16:55 -0600

    Adding assignment-1.4.js

---
+++ week-1/assignment-1.4.js
@@ -0,0 +1,34 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+var firstName; 
+firstName = "Andrea";
+var lastName = "Williams";
+var age = "29";
+var street = "124 Conch Street";
+var city = "Bikini Bottom";
+var state = "Pacific Ocean";
+var zipCode = "12444"
+
+// output 
+console.log(firstName); 
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(state);
+console.log(zipCode);
+
+
+// end program 
