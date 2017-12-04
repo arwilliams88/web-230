92326df7d3d7cf4d68c8ecbb95c32697fcc35fd7
Andrea Williams <andwilliams@bellevue.edu>
2017-12-03 21:58:23 -0600

    Adding assignment-1.5.js

---
+++ week-1/assignment-1.5.js
@@ -0,0 +1,105 @@
+/*
+============================================
+; Title:  Assignment 1.5
+; Author: Andrea Williams
+; Date:   3 December 2017
+; Modified By: Andrea Williams
+; Description: This program demonstrates the
+;   use of JavaScript types, values, and 
+;   and variables in an application.
+;===========================================
+*/ 
+
+/*
+    The code below builds a properly formatted
+    header and must be included in all applications
+    you write. In subsequent week's we will build this
+    functionality into a function and place it in a separate file.
+*/
+
+var myFirstName = "Andrea";
+var myLastName = "Williams";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+    + assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
+console.log("\n"); 
+
+// start of program - your code goes below this line
+
+var firstName = "First Name: Gwendolyn";
+var middleName = "Middle Name: Renae";
+var lastName = "Last Name: Williams";
+var address = "Address: 125 Conch Street Bikini Bottom, Pacific Ocean"
+var payRate = "Pay Rate: $12.50"
+var hireDate = "Hire Date: 2010-1-1"
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(address);
+console.log(payRate);
+console.log(hireDate);
+
+var firstName = "First Name: Brianna";
+var middleName = "Middle Name: Nicole";
+var lastName = "Last Name: Williams";
+var address = "Address: 126 Conch Street Bikini Bottom, Pacific Ocean"
+var payRate = "Pay Rate: $9.50"
+var hireDate = "Hire Date: 2012-1-12"
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(address);
+console.log(payRate);
+console.log(hireDate);
+
+var firstName = "First Name: Michaela";
+var middleName = "Middle Name: Joelle";
+var lastName = "Last Name: Jackson";
+var address = "Address: 129 Conch Street Bikini Bottom, Pacific Ocean"
+var payRate = "Pay Rate: $7.50"
+var hireDate = "Hire Date: 2012-12-12"
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(address);
+console.log(payRate);
+console.log(hireDate);
+
+var firstName = "First Name: Mark";
+var middleName = "Middle Name: Ray";
+var lastName = "Last Name: Robinson";
+var address = "Address: 324 Seashell Street Bikini Bottom, Pacific Ocean"
+var payRate = "Pay Rate: $19.50"
+var hireDate = "Hire Date: 2017-10-12"
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(address);
+console.log(payRate);
+console.log(hireDate);
+
+var firstName = "First Name: Jonathan";
+var middleName = "Middle Name: David";
+var lastName = "Last Name: Jackson";
+var address = "Address: 591 Starfish Lane Bikini Bottom, Pacific Ocean"
+var payRate = "Pay Rate: $14.00"
+var hireDate = "Hire Date: 2006-5-20"
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(address);
+console.log(payRate);
+console.log(hireDate);
+
+
+
+// end of program 
